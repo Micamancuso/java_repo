6f6d2a4f355891f90929693f2e1310e15f93c9e7
Micamancuso <85799596+Micamancuso@users.noreply.github.com>
2021-08-05 19:33:48 -0300

    SEGUNDA ENTREGA

--- main.js
+++ main.js
@@ -1,14 +1,43 @@
-let nombre= prompt ("ingrese su nombre")
-let apellido=  prompt ("ingrese su apellido")
-console.log (nombre+ " "+ apellido);
-let fecha= prompt ("ingrese año actual")
-let nacimiento= prompt ("ingrese su año de nacimiento")
-let edad= fecha-nacimiento
-console.log (edad)
-let verificacion= confirm ("¿tenes dni?")
-if (edad>=18 && verificacion){
-    console.log ("PODES ENTRAR");
-} else{
-    console.log ("NO SOS MAYOR")
-}
-alert ("¡Volve Pronto"+" "+ nombre+" "+ apellido+ "!")
+let passwordDefault = 311295
+let password = 0
+let count = 0
+let trys = 5
+let operacion
+let saldoActual = 5000
+let monto
+function sumar (monto) {
+    saldoActual = saldoActual + monto
+   }
+ 
+   function restar (monto) {
+     saldoActual = saldoActual - monto
+     }
+
+let usuario = prompt("ingresa tu usuario")
+alert(`el nombre que ingresaste es ${usuario}`);
+password = Number(prompt("ingrese la contraseña numerica"))
+if (password == passwordDefault) {
+    
+    operacion = Number(prompt ("¿Que operacion desea realizar? 1-deposito/ 2-extraccion"))
+    
+    
+    
+    switch (operacion) {
+        case 1 :
+        monto = Number(prompt("ingresa monto a depositar"))
+        sumar(monto)
+        alert("su saldo disponible es" + " "+ saldoActual);
+        break;
+    
+
+         case 2 :
+        monto = Number(prompt("ingresa monto a extraer"))
+        restar(monto)
+        alert ("su saldo disponible es" + " "+ saldoActual)
+        break;
+
+         default:
+         alert ("no se puede realizar esa operacion en este momento")
+        
+    }
+}else{ alert("CONTRASEÑA INCORRECTA")}
